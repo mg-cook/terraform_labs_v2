ec893455d7356757611366cc729102819d4d7813
matt <mg_cook@hotmail.com>
2024-12-09 16:58:11 +0000

    lab2

--- lab2/main.tf
+++ lab2/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.18.0"
     }
   }
@@ -28,8 +28,44 @@ resource "azurerm_subnet" "lab2" {
   name                 = "lab2-subnet"
   resource_group_name  = azurerm_resource_group.lab2.name
   virtual_network_name = azurerm_virtual_network.lab2.name
-  address_prefixes      = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-  # Do not edit above this line - The above code has been provided so that you may concentrate on building a VM and not be concerned with Provider, Resource Group or VNET at this time
+# Do not edit above this line - The above code has been provided so that you may concentrate on building a VM and not be concerned with Provider, Resource Group or VNET at this time
 
+resource "azurerm_network_interface" "lab2" {
+  name                = "lab2-nic"
+  location            = azurerm_resource_group.lab2.location
+  resource_group_name = azurerm_resource_group.lab2.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lab2.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "lab2-vm" {
+  name                = "VM2"
+  resource_group_name = azurerm_resource_group.lab2.name
+  location            = azurerm_resource_group.lab2.location
+  size                = "Standard_Ds2_v2"
+  admin_username      = "TPadminuser"
+  admin_password      = "TPP@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.lab2.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest" 
+  }
+  tags = {
+    Lab_edit = "SecondChange"
+  }
+}
