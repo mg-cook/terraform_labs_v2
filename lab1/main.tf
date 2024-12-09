ecfb0fc969bd2b23969146fad9938db48ab4dc31
matt <mg_cook@hotmail.com>
2024-12-09 14:57:19 +0000

    git lab1

---
+++ lab1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+            version = "3.0.1"
+        }
+    }
+}
+
+provider "docker" {
+    host = "tcp://localhost:2375"
+}
+
+# Pulls the image
+resource "docker_image" "apache_web" {
+    name = "httpd:latest"
+    }
+# Create a container
+resource "docker_container" "web_server" {
+    image = docker_image.apache_web.image_id
+    name = "web_server"
+    ports {
+        internal = 80
+        external = 88
+    }
+    }
